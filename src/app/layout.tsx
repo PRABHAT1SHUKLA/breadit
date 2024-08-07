b75ecd1e67736108475d86754262028eca790804
prabhat <prabhatshukla0849@gmal.com>
2024-08-07 17:04:24 +0530

    added authentication and error handling using toast components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import '@/styles/globals.css'
+import { cn } from '@/lib/utils'
+import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
+
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Breadit',
@@ -11,8 +18,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          <Navbar/>
+          {children}
+        </div>
+
+        <Toaster/>
+      </body>
     </html>
   )
 }
