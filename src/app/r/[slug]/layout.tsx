c994d8ab7a3ac3b09b70bf2357e7c0b54692c8fa
prabhat <prabhatshukla0849@gmail.com>
2024-08-19 17:08:16 +0530

    added create post functionality with uploadthing and editorjs

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -113,20 +113,14 @@ const Layout = async ({
                   variant: 'outline',
                   className: 'w-full mb-6',
                 })}
-                href={`r/${slug}/submit`}>
+                href={`${slug}/submit`}>
                 Create Post
               </Link>
             </dl>
-
-
-            </div>
+           </div>
           </div>
-        
-
-
         </div>
-       
-      </div>
+       </div>
 
 
     )
