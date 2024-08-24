ece4e878397d867d6af1fde49f943146424979f9
prabhat <prabhatshukla0849@gmail.com>
2024-08-24 20:34:25 +0530

    added server component to stream data using suspense while fetching posts info postserver component

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -71,7 +71,8 @@ export async function GET(req:Request){
             },
             where: whereClause,
         })
-    
+         console.log(`page ${page} being responsed`)
+         console.log(JSON.stringify(posts))
          return new Response(JSON.stringify(posts))
     } catch(error){
         return new Response('Could not fetch posts', { status: 500 })
