bd4c25319c38557c69abf4d35f986e31e283eb03
prabhat <prabhatshukla0849@gmail.com>
2024-08-24 21:40:29 +0530

    completed whole subreddit post page without comment added interactive suspense components for better user experience

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,9 +1,12 @@
+import EditorOutput from "@/components/EditorOutput"
+import PostVoteServer from "@/components/post-vote/PostVoteServer"
 import { buttonVariants } from "@/components/ui/Button"
 import { db } from "@/lib/db"
 import { redis } from "@/lib/redis"
+import { formatTimeToNow } from "@/lib/utils"
 import { CachedPost } from "@/types/redis"
 import { Post, User, Vote } from "@prisma/client"
-import { ArrowBigUp, Loader2 } from "lucide-react"
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react"
 import { notFound } from "next/navigation"
 import { Suspense } from "react"
 
@@ -40,7 +43,42 @@ const SubRedditPostPage = async ({ params }:
         <div>
             <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
                 <Suspense fallback={<PostVoteShell />}>
-                </Suspense>
+                  
+                  <PostVoteServer
+                    postId ={post?.id ?? cachedPost.id}
+                    getData={async() =>{
+                        return await db.post.findUnique({
+                            where:{
+                                id:params.postId,
+                            },
+                            include:{
+                                votes:true,
+                            },
+                        })
+                    }}
+                    />
+               </Suspense>
+
+               <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+                <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+                  Posted by <u />{post?.author.username ?? cachedPost.authorUsername}{' '}
+                  {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+                </p>
+                <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+                {post?.title ?? cachedPost.title}
+                </h1>
+               </div>
+                
+                <EditorOutput content={post?.content ?? cachedPost.content} />
+
+                {/* <Suspense 
+                  fallback={
+                    <Loader2 className='h-5 w-5 animate-spin text-zinc-500' />
+                  }>
+
+                  </Suspense> */}
+
+
 
             </div>
 
