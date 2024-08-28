8fa0c631c8dcfadb38da7f23bc67cc1fcf5bd2d0
prabhat <prabhatshukla0849@gmail.com>
2024-08-28 17:42:14 +0530

    added functional logic to fetch only top level comments in commentSection component

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,3 +1,5 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { Comment, CommentVote, User } from "@prisma/client";
 
 
@@ -17,5 +19,25 @@ interface CommentSectionProps {
 }
 
 const CommentsSection = async({postId}: CommentSectionProps) =>{
+
+    const session = await getAuthSession()
+
+    const comments = await db.comment.findMany({
+        where:{
+            postId:postId,
+            replyToId: null,
+        },
+        include:{
+            author: true,
+            votes: true,
+            replies:{
+                 include:{
+                    author: true,
+                    votes: true,
+                 },
+            },
+        },
+    })
     
+
 }
