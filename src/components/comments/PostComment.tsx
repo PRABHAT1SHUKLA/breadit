2183c2e12ef57f89870006deed82952cf5c4a389
prabhat <prabhatshukla0849@gmail.com>
2024-08-29 16:20:35 +0530

    working on postComment component jsx

--- src/components/comments/PostComment.tsx
+++ src/components/comments/PostComment.tsx
@@ -9,85 +9,110 @@ import { text } from "stream/consumers";
 import axios from "axios";
 import { UserAvatar } from "../UserAvatar";
 import { formatTimeToNow } from "@/lib/utils";
+import CommentVotes from "../CommentVotes";
+import { Button } from "../ui/Button";
+import { MessageSquare } from "lucide-react";
 
 
 type ExtendedComment = Comment & {
     votes: CommentVote[],
-    author: User 
+    author: User
 }
 
 interface PostCommentProps {
     comment: ExtendedComment
     votesAmt: number
-    currentVote : CommentVote | undefined
+    currentVote: CommentVote | undefined
     postId: string
 }
 
-const PostComment: FC<PostCommentProps> = ({comment, votesAmt, currentVote, postId}) =>{
-   
-    const { data : session } = useSession()
+const PostComment: FC<PostCommentProps> = ({ comment, votesAmt, currentVote, postId }) => {
 
-    const [ isReplying , setIsReplying] = useState<boolean>(false)
+    const { data: session } = useSession()
+
+    const [isReplying, setIsReplying] = useState<boolean>(false)
 
     const commentRef = useRef<HTMLDivElement>(null)
-    
-    const [input , setInput] = useState<string>(`@${comment.author.username}`)
+
+    const [input, setInput] = useState<string>(`@${comment.author.username}`)
 
     const router = useRouter()
 
-    useOnClickOutside(commentRef , () => {
+    useOnClickOutside(commentRef, () => {
         setIsReplying(false)
     })
 
-    const { mutate: PostComment , isLoading} =
-     useMutation({
-        mutationFn: async({ postId, text, replyToId} : CommentRequest) =>{
+    const { mutate: PostComment, isLoading } =
+        useMutation({
+            mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
 
-            const payload : CommentRequest = {
-                postId ,
-                text ,
-                replyToId
-            }
+                const payload: CommentRequest = {
+                    postId,
+                    text,
+                    replyToId
+                }
 
-         const {data} = await axios.patch(`/api/subreddit/post/comment/`, payload)
-         
-         return data
+                const { data } = await axios.patch(`/api/subreddit/post/comment/`, payload)
 
-        },
-        
-        onError: () => {
-            return toast({
-              title: 'Something went wrong.',
-              description: "Comment wasn't created successfully. Please try again.",
-              variant: 'destructive',
-            })
-          },
-          onSuccess: () => {
-            router.refresh()
-            setIsReplying(false)
-          },
+                return data
+
+            },
+
+            onError: () => {
+                return toast({
+                    title: 'Something went wrong.',
+                    description: "Comment wasn't created successfully. Please try again.",
+                    variant: 'destructive',
+                })
+            },
+            onSuccess: () => {
+                router.refresh()
+                setIsReplying(false)
+            },
 
 
-     })
+        })
 
-    return(<div ref={commentRef} className="flex flex-col">
-       <div className="flex items-center">
-        <UserAvatar
-          user={{
-            name: comment.author.name || null,
-            image: comment.author.image || null,
-          }}
-          className='h-6 w-6'
-        />
-         <div className='ml-2 flex items-center gap-x-2'>
-          <p className='text-sm font-medium text-gray-900'>u/{comment.author.username}</p>
+    return (<div ref={commentRef} className="flex flex-col">
+        <div className="flex items-center">
+            <UserAvatar
+                user={{
+                    name: comment.author.name || null,
+                    image: comment.author.image || null,
+                }}
+                className='h-6 w-6'
+            />
+            <div className='ml-2 flex items-center gap-x-2'>
+                <p className='text-sm font-medium text-gray-900'>u/{comment.author.username}</p>
 
-          <p className='max-h-40 truncate text-xs text-zinc-500'>
-            {formatTimeToNow(new Date(comment.createdAt))}
-          </p>
+                <p className='max-h-40 truncate text-xs text-zinc-500'>
+                    {formatTimeToNow(new Date(comment.createdAt))}
+                </p>
+            </div>
         </div>
-       </div>
- 
+
+        <p className="text-sm text-zinc-900 mt-2"> {comment.text}</p>
+
+        <div className="flex gap-2 items-center">
+            <CommentVotes
+                commentId={comment.id}
+                votesAmt={votesAmt}
+                currentVote={currentVote}
+            />
+
+            <Button
+                onClick={() => {
+                    if (!session) return router.push('/sign-in')
+                    setIsReplying(true)
+                    variant = 'ghost'
+                    size = 'xs'
+                }}>
+
+                <MessageSquare className='h-4 w-4 mr-1.5' />
+                Reply
+            </Button>
+        </div>
+            
 
     </div>)
 }
