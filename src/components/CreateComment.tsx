3a4977319abb2efbb2fffb1215148f7cae310443
prabhat <prabhatshukla0849@gmail.com>
2024-08-29 08:15:50 +0530

    createcomment component implemented fullywith the ui features

--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -7,6 +7,9 @@ import axios from "axios"
 
 import { useRouter } from "next/router"
 import { FC, useState } from "react"
+import { Label } from "./ui/Label"
+import { Textarea } from "./ui/Textarea"
+import { Button } from "./ui/Button"
 
 
 interface CreateCommentProps {
@@ -54,4 +57,38 @@ const CreateComment: FC<CreateCommentProps>=({
 
     })
 
+    return (
+         
+         <div className="grid w-full gap-1.5">
+          <Label htmlFor='comment'>
+            Your Comment
+          </Label>
+
+          <div className="mt-2">
+            <Textarea
+            id="comment"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows={1}
+            placeholder="share your thoughts"/>
+          <div className="mt-2 flex justify-end">
+           <Button
+              isLoading={isLoading}
+              disabled={input.length == 0}
+              onClick={() =>  comment({postId, text:input , replyToId})}>
+                Post
+              </Button>
+
+          </div>
+
+
+          </div>
+
+         </div>
+     
+
+    )
+
 }
+
+export default CreateComment
