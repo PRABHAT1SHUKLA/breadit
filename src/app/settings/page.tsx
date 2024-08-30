e92f114e1e280605da69e2ca2a985589e99071dd
prabhat <prabhatshukla0849@gmail.com>
2024-08-30 16:26:57 +0530

    some debugging for username functionality

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,21 +1,21 @@
-import { authOptions, getAuthSession } from "@/lib/auth"
-import { redirect } from "next/navigation"
-import { UserNameForm } from '@/components/UserNameForm'
+import { redirect } from 'next/navigation'
 
-export const metadata ={
-    title: 'Settings',
-    description: 'Manage account and website settings.',
+import { UserNameForm } from '@/components/UserNameForm'
+import { authOptions, getAuthSession } from '@/lib/auth'
+
+export const metadata = {
+  title: 'Settings',
+  description: 'Manage account and website settings.',
 }
 
-export default async function SettingsPage(){
-    const session = await getAuthSession()
+export default async function SettingsPage() {
+  const session = await getAuthSession()
 
-    if(!session?.user){
-        redirect(authOptions?.pages?.signIn || '/login')
-    }
+  if (!session?.user) {
+    redirect(authOptions?.pages?.signIn || '/login')
+  }
 
-
-return (
+  return (
     <div className='max-w-4xl mx-auto py-12'>
       <div className='grid items-start gap-8'>
         <h1 className='font-bold text-3xl md:text-4xl'>Settings</h1>
@@ -30,6 +30,5 @@ return (
         </div>
       </div>
     </div>
-
-)
+  )
 }
